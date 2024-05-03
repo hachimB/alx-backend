4c21e816f371de45644e96bdbc8124a7e34d72d9
hachimB <boubacarhachim@gmail.com>
2024-05-04 00:09:49 +0100

    get page

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,9 +32,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
-            index = index_range(page, page_size)
-            if not index:
-                 return []
-            return self.dataset()[index[0]:index[1]]
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        index = index_range(page, page_size)
+        if not index:
+            return []
+        return self.dataset()[index[0]:index[1]]
