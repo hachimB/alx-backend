94634d3d9d7210c9115e0a2d1e62f90fb122e463
hachimB <boubacarhachim@gmail.com>
2024-05-04 01:51:03 +0100

    3-hypermedia_del_pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """get_hyper_index"""
         assert isinstance(
             index, int) and 0 <= index < len(
             self.indexed_dataset())
