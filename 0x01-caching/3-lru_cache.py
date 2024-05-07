f3a2b6795bc172a5e5b6e7f12607982956994d85
hachimB <boubacarhachim@gmail.com>
2024-05-08 00:24:15 +0100

    LRU(Least Recently Used)

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module documentation"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """FIFOCache class"""
+
+    def __init__(self):
+        """init constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """puts keys and values in self.cache_data dictionnary"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            left_key = next(iter(self.cache_data.keys()))
+            self.cache_data.pop(left_key)
+            print("DISCARD: {}".format(left_key))
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """gets the value of a key in self.cache_data dictionnary"""
+        if key is None or not self.cache_data.get(key):
+            return None
+        return self.cache_data[key]
