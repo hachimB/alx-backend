64c23ea2be68b2f106d8d4280fba7cb7021daeb3
hachimB <boubacarhachim@gmail.com>
2024-05-08 00:08:09 +0100

    Lifo

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module documentation"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """FIFOCache class"""
+
+    def __init__(self):
+        """init constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """puts keys and values in self.cache_data dictionnary"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = list(self.cache_data.keys())[-1]
+            self.cache_data.pop(last_key)
+            print("DISCARD: {}".format(last_key))
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """gets the value of a key in self.cache_data dictionnary"""
+        if key is None or not self.cache_data.get(key):
+            return None
+        return self.cache_data[key]
