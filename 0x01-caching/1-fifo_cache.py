17b550328c7fc84346aa64e901bee2bbbe4911d8
hachimB <boubacarhachim@gmail.com>
2024-05-07 21:20:11 +0100

    fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module documentation"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache class"""
+
+    def __init__(self):
+        """init constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """puts keys and values in self.cache_data dictionnary"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) > self.MAX_ITEMS:
+            first_out = self.cache_data.pop(list(self.cache_data.keys())[0])
+            print(first_out)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """gets the value of a key in self.cache_data dictionnary"""
+        if key is None or not self.cache_data.get(key):
+            return None
+        return self.cache_data[key]
