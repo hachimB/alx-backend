9460cfecd72ebba6eee9d2cd25caac54ba05407e
hachimB <boubacarhachim@gmail.com>
2024-05-08 01:34:03 +0100

    LRU(Least Recently Used)

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,6 +3,28 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 
+# class LRUCache(BaseCaching):
+#     """LRUCache class"""
+
+#     def __init__(self):
+#         """init constructor"""
+#         super().__init__()
+
+#     def put(self, key, item):
+#         """puts keys and values in self.cache_data dictionnary"""
+#         if key is None or item is None:
+#             return
+#         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+#             left_key = next(iter(self.cache_data.keys()))
+#             self.cache_data.pop(left_key)
+#             print("DISCARD: {}".format(left_key))
+#         self.cache_data[key] = item
+
+#     def get(self, key):
+#         """gets the value of a key in self.cache_data dictionnary"""
+#         if key is None or not self.cache_data.get(key):
+#             return None
+#         return self.cache_data[key]
 class LRUCache(BaseCaching):
     """LRUCache class"""
 
@@ -11,7 +33,7 @@ class LRUCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """puts keys and values in self.cache_data dictionnary"""
+        """puts keys and values in self.cache_data dictionary"""
         if key is None or item is None:
             return
         if key in self.cache_data:
@@ -23,7 +45,9 @@ class LRUCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """gets the value of a key in self.cache_data dictionnary"""
-        if key is None or not self.cache_data.get(key):
+        """gets the value of a key in self.cache_data dictionary"""
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data[key]
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
