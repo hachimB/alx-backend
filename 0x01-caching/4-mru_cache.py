20f05c978d338a5b87924d66e9189d6f840538db
hachimB <boubacarhachim@gmail.com>
2024-05-08 02:00:42 +0100

    MRU(Most Recently Used)

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -9,21 +9,22 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """init constructor"""
         super().__init__()
+        self.mru_key = None
 
     def put(self, key, item):
         """puts keys and values in self.cache_data dictionary"""
         if key is None or item is None:
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            left_key = next(iter(self.cache_data.keys()))
-            self.cache_data.pop(left_key)
-            print("DISCARD: {}".format(left_key))
+            self.cache_data.pop(self.mru_key)
+            print("DISCARD: {}".format(self.mru_key))
         self.cache_data[key] = item
+        self.mru_key = key
 
     def get(self, key):
         """gets the value of a key in self.cache_data dictionary"""
         if key is None or key not in self.cache_data:
             return None
-        value = self.cache_data.pop(key)
-        self.cache_data[key] = value
-        return value
+        self.mru_key = key
+        return self.cache_data[key]
+
