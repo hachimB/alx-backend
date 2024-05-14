4927cacc6913511c99fe2f0e038dc4827cd747b6
hachimB <boubacarhachim@gmail.com>
2024-05-14 11:10:42 +0100

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,8 +8,8 @@ from flask_babel import Babel
 class Config():
     """Config class"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    # BABEL_DEFAULT_LOCAL = "en"
+    # BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -17,5 +17,5 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
