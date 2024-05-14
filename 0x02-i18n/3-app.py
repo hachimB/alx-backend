4f61f051e8ebca7b29633cc8b71f97db052208e6
hachimB <boubacarhachim@gmail.com>
2024-05-14 20:12:24 +0100

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -12,12 +12,12 @@ babel = Babel(app)
 @app.route("/")
 def home() -> str:
     """function to parametrize our templates"""
-    # home_title = gettext("home_title")
-    # home_header = gettext("home_header")
+    home_title = gettext("home_title")
+    home_header = gettext("home_header")
     return render_template(
         '3-index.html',
-        # home_title=home_title,
-        # home_header=home_header,
+        home_title=home_title,
+        home_header=home_header,
         lang=get_locale())
 
 
