9bd23c84a0f336c66078bac7452edf643706fd1d
hachimB <boubacarhachim@gmail.com>
2024-05-14 12:31:30 +0100

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Parametrize templates
+"""
+from flask import Flask, render_template
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route("/")
+def home():
+    """function to parametrize our templates"""
+    home_title = gettext("home_title")
+    home_header = gettext("home_header")
+    return render_template(
+        '3-index.html',
+        home_title=home_title,
+        home_header=home_header)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
