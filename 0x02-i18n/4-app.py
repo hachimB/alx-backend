215f659a13d2ca417c883d6ec1ccf71972398c81
hachimB <boubacarhachim@gmail.com>
2024-05-15 00:48:21 +0100

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
-"""Force locale with URL parameter
-"""
-from flask import Flask
+"""Parametrize templates"""
+from flask import request, Flask, render_template
 from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """"get_local
+    """
+    locale = request.args.get('locale')
+    if locale in ['en', 'fr']:
+        return locale
+    return request.accept_languages.best_match(['en', 'fr'])
+
+
+@app.route("/")
+def home() -> str:
+    """function to parametrize our templates"""
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
